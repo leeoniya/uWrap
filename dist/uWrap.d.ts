bf18faaea5ef39bfc1dbdc6ca49be491d3bf5ead
Leon Sorokin <leeoniya@gmail.com>
2025-04-05 16:37:13 -0500

    fix types

--- dist/uWrap.d.ts
+++ dist/uWrap.d.ts
@@ -2,7 +2,7 @@
 export type LineCallback = (idx0: number, idx1: number) => void | boolean;
 
 /** invoke callback for each line with start/end idxs */
-export type Each  = (text: string, width: number, cb: LineCallback) => void;
+export type Each = (text: string, width: number, cb: LineCallback) => void;
 
 /** split into lines array */
 export type Split = (text: string, width: number, limit?: number) => string[];
@@ -14,11 +14,11 @@ export type Count = (text: string, width: number) => number;
 export type Test = (text: string, width: number) => boolean;
 
 export interface uWrap {
-    each: Each;
-    split: Split;
-    count: Count;
-    test: Test;
-};
+  each: Each;
+  split: Split;
+  count: Count;
+  test: Test;
+}
 
 /** wrap text with a variable width font using pre-line whitespace strategy */
-export function varPreLine(ctx: CanvasRenderingContext2D): Wrapper;
+export function varPreLine(ctx: CanvasRenderingContext2D): uWrap;
